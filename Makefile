f3381022d7aff521f6072d7e054ee006d1dd4b2f
Mudream <mudream4869@gmail.com>
2017-12-17 23:52:30 +0800

    add many variable test

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: examples
 
-examples: examples/basic-calculation.out examples/interior-point-method.out examples/gauss-newton.out examples/levenberg-marquardt.out
+examples: examples/many_variables.out examples/basic-calculation.out examples/interior-point-method.out examples/gauss-newton.out examples/levenberg-marquardt.out
 
 %.out: %.cpp Derivative.cpp Derivative.h
 	g++ $< -o $@ -I eigen/ -I . -std=c++11
---
+++ examples/many_variables.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <chrono>
+#include "Derivative.cpp"
+
+using Eigen::Derivative;
+
+struct Timer{
+    std::chrono::steady_clock::time_point t1;
+    Timer(){
+        t1 = std::chrono::steady_clock::now();
+    }
+
+    double operator()(){
+        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+        return std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+    }
+};
+    
+
+int main(){
+
+    // Variable Derivative
+
+    const int N = 100;
+    
+    Derivative p = 1;
+    for(int lx = 0;lx < N;lx++)
+        p = p*Derivative::Variable(lx);
+
+    VectorXd v = VectorXd::Random(N);
+
+    Timer t1; 
+    auto dp = p.diffPartial(0);
+    std::cout << "Partial differential cost " << t1() << "s" << std::endl;
+
+    Timer t2;
+    std::cout << dp(v) << std::endl; 
+    std::cout << "Calculate value cost " << t2() << "s" << std::endl;
+
+    Timer t3;
+    for(int lx = 0;lx < N;lx++)
+        p = p.diffPartial(lx);
+    std::cout << "Partial diff all variables cost " << t3() << "s" << std::endl;
+
+    return 0;
+}
